0f9ae55a0586fc81f514624ae12fbedf5ed591fd
Eliza Sohn and Peter Wells <peter@>
2019-11-06 10:40:26 -0800

    add start of a game loop, scaffolding for a player turn function

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,13 @@
 function Player(name, totalScore, turnScore) {
   this.name = name;
   this.totalScore = totalScore;
-  this.turnScore = turnScore;
 }
 
 Player.prototype.addTotalScore = function(value) {
   this.totalScore += value;
 }
 
-Player.prototype.addTurnScore = function(value) {
-  this.turnScore += value;
-}
+
 
 //Dice Logic ------------------------
 function diceRoll() {
@@ -19,11 +16,45 @@ function diceRoll() {
 }
 
 //Play game -------------------------
+function addScore(player) {
+
+}
+
 function playGame(player1, player2) {
+  var player1Turn = true;
+  var hold = false;
+  var turnScore = 0;
+
+  while ((player1.totalScore < 100) && (player2.totalScore < 100)) {
+    while (player1Turn === true){
+      while (hold === false) {
+      //listen for rolls, add turnscore so long as its not a 1 or if hold has been pressed
+      if (//roll === 1) {
+        turnScore = 0;
+        player1Turn = false;
+      }
+      else if (//roll > 1) {
+        player1.addTotalScore(turnScore);
+        turnScore = 0;
+      }
+      //hold has been pressed
+      player1.addTotalScore(turnScore);
+      turnScore = 0;
+      player1Turn = false;
+    }
+    
+  }
+
+
+
+  } //End Game Loop ----------------
+  console.log("game overrrrrrrr!!!");
+  return;
 
 }
 
 //Initialize Variables --------------
 var newPlayer = new Player("Jimmitohy", 0, 0);
+var newPlayer2 = new Player("Eliza", 0, 0);
 var roll = diceRoll();
 console.log(diceRoll());
