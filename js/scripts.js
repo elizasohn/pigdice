b6e4503d11ee223423a05f23c3cad19fe7b9f4c5
Eliza Sohn and Peter Wells <peter@>
2019-11-06 14:46:35 -0800

    adds part of the interface and makes global player variables that can be defined.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
 //Initialize Variables --------------
-var newPlayer = new Player("Jimmitohy", 0, 0);
-var newPlayer2 = new Player("Eliza", 0, 0);
+// var newPlayer = new Player("Jimmitohy", 0, 0);
+// var newPlayer2 = new Player("Eliza", 0, 0);
+var playerA;
+var playerB;
 
-var hold = false;
-var roll = false;
 var diceValue = 0;
 var turnScore = 0;
+var isOne = false;
+var hold = false;
 
 //Player Logic ----------------------
 function Player(name, totalScore) {
@@ -23,32 +25,19 @@ function diceRoll() {
 }
 
 //Player Turn Logic -----------------
-function playerTurn(activePlayer) {
-  turnScore = 0;
+function playerTurn() {
+  var diceValue = diceRoll();
+  console.log("You rolled: ", diceValue);
+  if (diceValue === 1) {
+    turnScore = 0;
+    isOne = true;
 
-  while ( (roll === true) && (hold === false) ) {
-
-    var diceValue = diceRoll();
-    console.log(activePlayer, " rolled a: ", diceValue);
-
-    //Check the value of the Dice
-    if (diceValue === 1) {
-      turnScore = 0;
-      roll = false;
-      console.log("Rolled a 1. Score: ", turnScore);
-
-    } else if (diceValue > 1) {
-        turnScore += diceValue;
-        console.log("Current Value in Turn", diceValue);
-        roll = false;
-    }
-
+  } else if (diceValue > 1) {
+    turnScore += diceValue;
   }
-  console.log("Returning: ", turnScore);
-  roll = false;
-  hold = false;
   return turnScore;
 }
+
 
 
 
@@ -60,34 +49,59 @@ function playGame(player1, player2) {
   while ((player1.totalScore < 100) && (player2.totalScore < 100)) {
     //Checks to see if it's player 1's turn
     while (player1Turn === true){
+      if (hold === true) {
         player1.addTotalScore(playerTurn(player1));
         player1Turn = false;
-        console.log(player1);
+        console.log("player1: ", player1.totalScore);
+      } else if (isOne === true) {
+        turnScore = 0;
+        player1Turn = false;
+        console.log("player1 no points for you: ", player1.totalScore);
       }
-        //Else if it's player 2's turn
-        player2.addTotalScore(playerTurn(player2));
-        player1Turn = true;
-        console.log(player2);
-        //Goes back and checks total scores --^
     }
-    //End Game
+    if (hold === true) {
+      player1.addTotalScore(playerTurn(player1));
+      player1Turn = true;
+      console.log("player1: ", player1.totalScore);
+    } else if (isOne === true) {
+      turnScore = 0;
+      player1Turn = true;
+      console.log("player1 no points for you: ", player1.totalScore);
+    }
+
+  } //Close While Loop
+
     console.log("game over!")
     return;
   } //closes play game function
 
 
 
-// $(document).ready(function(){
-//   // Collect User Names
-//   // var player1name = //jquery
-//   // var player2name = //jquery
+$(document).ready(function(){
+  // $("form#userRegForm").submit(function(event){
+    // event.preventDefault();
+
+    $("#enterUser").click(function(){
+      var player1nameInput = $("#player1").val();
+      var player2nameInput = $("#player2").val();
+      playerA = new Player(player1nameInput, 0);
+      playerB = new Player(player2nameInput, 0);
+    });
+
+    playGame(playerA, playerB);
+    //
+    // $(".player1Name").text(player1nameInput);
+    // $(".player2Name").text(player2nameInput);
+    //
+    // $(".currentPlayer").text(player1nameInput);
+
+  });
+
+
 //
-//
-//
-//
-//   // Play game
-//   playGame(player1, player2);
-//
-//   // Determine Winner / Display that
-//
+//   $("#rollBtn").on("click", "button", function(){
+//     roll = true;
+//     playerTurn();
+//     roll = false;
+//   });
 // });
