796d01391cdbd8ac3c1807a59dec7f37b3079350
Eliza Sohn and Peter Wells <eliza@>
2019-11-07 13:15:28 -0800

    fixed it.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,16 @@
 //Initialize Variables --------------
-// var newPlayer = new Player("Jimmitohy", 0, 0);
-// var newPlayer2 = new Player("Eliza", 0, 0);
 var player1;
 var player2;
+var turn = true;
+
 
 //Player Logic ----------------------
-function Player(id, name, totalScore, tempScore) {
+function Player(id, name, turn) {
   this.id = id;
   this.name = name;
-  this.totalScore = totalScore;
-  this.tempScore = tempScore;
+  this.turn = turn;
+  this.totalScore = 0;
+  this.tempScore = 0;
 }
 
 Player.prototype.addTotalScore = function(value) {
@@ -26,92 +27,100 @@ function diceRoll() {
   return Math.floor(Math.random() * 6) + 1;
 }
 
-//Player Turn Logic -----------------
-function playerTurn(diceValue) {
-  console.log("You rolled: ", diceValue);
-  if (diceValue === 1) {
-    tempScore = 0;
-    isOne = true;
 
-  } else if (diceValue > 1) {
-    tempScore += diceValue;
+//rolling dice, checking turn and adding the value to the tempScore
+function playerRoll(player) {
+    var diceValue = diceRoll();
+    console.log(player.name, " rolled: ", diceValue);
+    if (diceValue === 1) {
+      player.tempScore = 0;
+      turn = !turn;
+      console.log(turn);
+    } else if (diceValue > 1) {
+      player.tempScore += diceValue;
+    }
+    else {
+      return player.tempScore;
+    }
+}
+
+// hold to totalScore function
+function hold(player){
+  player.addTotalScore(player.tempScore);
+  player.tempScore = 0;
+  turn = !turn;
+  if (player.totalScore > 100){
+    console.log("Yay! "+ player.name + " wins!!!");
   }
-  return tempScore;
+  console.log(player.name, " score:", player.totalScore);
+}
+
+function turnCheck() {
+  if (turn === true){
+    player1.turn = true;
+    player2.turn = false;
+    return true;
+  }else if (turn === false){
+    player1.turn = false;
+    player2.turn = true;
+    return false;
+  }
 }
 
 
-// Game Loop Logic ------------------
-function playGame(player1, player2) {
-  var player1Turn = true;
-
-  //Checks to see if a player has 100
-    if ((player1Turn === true) && (hold === true)){
-        player1.addTotalScore(playerTurn());
-        player1Turn = false;
-        hold = false;
-        console.log("player1: ", player1.totalScore);
-    } else if (isOne === true) {
-      tempScore = 0;
-      player1Turn != player1Turn;
-      console.log("player1 no points for you: ", player1.totalScore);
-    }
-    //Player 2's Turn:
-     else if ((player1Turn === false) && (hold === true)) {
-       player2.addTotalScore(playerTurn());
-       player1Turn = true;
-       hold = false;
-       console.log("player2: ", player2.totalScore);
-    }
-  // }
-
-    console.log("game over!")
-    return;
-  } //closes play game function
-
-
-
+// Front End Logic ------------------------------------
 $(document).ready(function(){
 
-    $("#enterUser").click(function(){
-      var player1nameInput = $("#player1").val();
-      var player2nameInput = $("#player2").val();
-      player1 = new Player(1, player1nameInput, 0, false, false, 0);
-      player2 = new Player(2, player2nameInput, 0, false, false, 0);
-      $(".userRegBox").slideUp();
-    });
-
-//THIS IS THE PLAN!!! Show and Hide different UserInputBox divs to get unique inputs for the player objects!!
-//Get it to count into the object with roll button and add that to player objects
-//Worry about hitting 1's later
-
-    $(".rollBtn").click(function(){
-      console.log("click");
-      // var rollResult = diceRoll();
-      // console.log(rollResult);
-      // var id = this.id;
-      // console.log(id);
-      // if (id === 1){
-      //   player1.addTurnScore(playerTurn(rollResult));
-      // } else {
-      //   player2.addTurnScore(playerTurn(rollResult));
-    });
+  $("#enterUser").click(function(){
+    var player1nameInput = $("#player1").val();
+    var player2nameInput = $("#player2").val();
+    player1 = new Player(1, player1nameInput, true);
+    player2 = new Player(2, player2nameInput, false);
+    $(".userRegBox").slideUp();
+    updateScreens();
+  });
 
 
+  $(".rollBtn").click(function(){
+    if (turnCheck() === true){
+      playerRoll(player1);
+      updateScreens();
+    } else if (turnCheck() === false){
+      playerRoll(player2);
+      updateScreens();
+    }
+  });
 
-    });
+  $("#holdBtn").click(function(){
+    if (turnCheck() === true){
+      hold(player1);
+      if (player1.totalScore > 100){
+        winGame(player1);
+      }
+    } else if (turnCheck() === false){
+      hold(player2);
+      if (player1.totalScore > 100){
+        winGame(player1);
+      }
+    }
+    updateScreens();
+  });
 
-    //
-    // $(".player1Name").text(player1nameInput);
-    // $(".player2Name").text(player2nameInput);
-    //
-    // $(".currentPlayer").text(player1nameInput);
+  function updateScreens(){
+    $(".player1Name").text(player1.name);
+    $(".player2Name").text(player2.name);
+    $(".player1Score").text(player1.totalScore);
+    $(".player2Score").text(player2.totalScore);
+    if (turnCheck() === true){
+      $(".currentPlayer").text(player1.name);
+      $("#currentTempScore").text(player1.tempScore);
+    } else if (turnCheck() === false){
+      $(".currentPlayer").text(player2.name);
+      $("#currentTempScore").text(player2.tempScore);
+    }
+  }
 
+  function winGame(){
+  }
 
-
-//
-//   $("#rollBtn").on("click", "button", function(){
-//     roll = true;
-//     playerTurn();
-//     roll = false;
-//   });
-// });
+});
